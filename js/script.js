e73bc9c57e88ad72cd2b338ef03e0c8947b0d089
YanTP2 <yantpantoja2@gmail.com>
2026-04-07 16:10:35 -0300

    Iniciando função de grupos de horários no script.js

--- js/script.js
+++ js/script.js
@@ -98,6 +98,20 @@ function getUserLocation(options = {}) {
   });
 }
 
+//Sistema de cor dinâmica
+
+function getTimeGroup(hour) {
+  if (hour >= 6 && hour < 12) {
+    return "morning";
+  } else if (hour >= 12 && hour < 18) {
+    return "afternoon";
+  } else if (hour >= 18 && hour < 24) {
+    return "night";
+  } else {
+    return "dawn";
+  }
+}
+
 /*
 
 Localização ex: Erechim, RS
